41bcc7c072f8ca6684e5edd513b72451d0a72e4a
Andrew Davis <andrew.bradley.davis@gmail.com>
2018-04-25 16:07:09 -0400

    filter

--- src/Card.js
+++ src/Card.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 const Card = ({ id, name, email }) => {
 	return (
-		<div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
+		<div className='f6 tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
 			<img alt='robots' src={`https://robohash.org/${id}?size=200x200`} />
 			<div>
-				<h4>{name}</h4>
+				<h3>{name}</h3>
 				<p>{email}</p>
 			</div>
 		</div>
